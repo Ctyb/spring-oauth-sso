ea3c70cfb1ef6ca6eb8530af12a221b72cfd2db5
luojingyu <whaleluo@163.com>
2019-05-24 13:39:33 +0800

    client1

---
+++ sso-client1/src/main/java/com/whale/sso/client/SsoClient1Application.java
@@ -0,0 +1,34 @@
+package com.whale.sso.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.Serializable;
+
+/**
+ * @author ljy
+ * @version V1.0
+ * @Package PACKAGE_NAME
+ * @Description: TODO
+ * @date 2019/5/24 0024 10:53
+ */
+
+@SpringBootApplication
+@RestController
+@EnableOAuth2Sso
+public class SsoClient1Application  {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SsoClient1Application.class,args);
+    }
+
+    @GetMapping("/user")
+    public Authentication user(Authentication user){
+        return user;
+    }
+
+}
